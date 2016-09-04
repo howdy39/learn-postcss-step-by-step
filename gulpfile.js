bec16b99991667b15dd8c1fcd1c3e22f71fb1585
Tatsuya.Nakano <admin@howdylikes.jp>
2016-09-04 17:32:17 +0900

    finished step4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
 
 gulp.task('build', function () {
+  var postcss = require('gulp-postcss');
+  var customProperties = require('postcss-custom-properties');
+  var nested = require('postcss-nested');
+
+  var preProcessors = [
+    customProperties
+    , nested
+  ];
+
   return gulp.src('./css/app.css')
+    .pipe(postcss(preProcessors))
     .pipe(gulp.dest('./dist'));
 });
